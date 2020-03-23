c5dc2b4abe2f2c4e8a74711ec09bd170f86d49fd
alceil <ashishshajisrampickal@gmail.com>
2020-03-23 10:36:47 +0530

    else

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
-alert('wubba lubba dub dub');
+var kindiage=29;
+if(kindiage>30)
+{
+    document.write("wubba lubba dub dub");
+}
+else if(kindiage>20)
+{
+    document.write('rickity rick!');
+}
+else if(kindiage>10)
+{
+    document
+}
