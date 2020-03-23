17615d70b5860bb084e8ff22ec96fdf3ed322439
alceil <ashishshajisrampickal@gmail.com>
2020-03-23 10:59:54 +0530

    added for loop

--- test.js
+++ test.js
@@ -1,6 +1,5 @@
-var age=5;
-while(age)
+for(age=5;age<10;age++)
 {
-    console.log('you are less than 10 kid ')
+    console.log('you are less than 10 kid ');
 }
-document.write('you are over 10 now kid')
+document.write('you are over 10 now kid');
