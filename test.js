a1e652e304a2fac37f04ee3aafa0b7c0f75d4efe
alceil <ashishshajisrampickal@gmail.com>
2020-03-23 12:47:24 +0530

    added ninja index of

--- test.js
+++ test.js
@@ -1,6 +1,9 @@
-var hey ='I\'m a "fun kid" okay';
+var hey ='I\'m a "fun ninja" okay';
 if(hey.indexOf("ninja")==-1)
 {
     console.log("there is no element such as ninja");
 }
-
+else
+{
+    console.log("ninja starts at position" + hey.indexOf('ninja'));
+}
