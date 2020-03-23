aeb32c957abe2bd28c28b1db18f8cd6b7a16e011
alceil <ashishshajisrampickal@gmail.com>
2020-03-23 11:19:32 +0530

    added break

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
-var links=document.getElementsByTagName("a");
-for(i=1;i<=links.length;i++)
+for(i=1;i<10;i++)
 {
-    console.log('this is link no'+i);
+    
+    console.log(i);
+    if(i==7)
+    {
+        break;
+    }
 }
 document.write('all links are now looped');
