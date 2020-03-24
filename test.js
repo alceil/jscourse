10b07eb89d0d1bdcec322ceffad22729520338b8
alceil <ashishshajisrampickal@gmail.com>
2020-03-24 11:19:04 +0530

    error pgm

--- test.js
+++ test.js
@@ -3,3 +3,10 @@ mycar.maxspeed=50;
 mycar.driver="shaun";
 mycar.drive=function(){console.log("now he is driving");};
 mycar.drive;
+var mycar2
+{
+    maxspeed:50;
+    driver: "shaun";
+    drive: function () {console.log("now he is driving");};
+};
+mycar2.drive
